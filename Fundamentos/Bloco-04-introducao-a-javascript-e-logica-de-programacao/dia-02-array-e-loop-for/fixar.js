4b70c761310e2e44d11dd90c033130cfe6139819
Victor Roesner <victorroesner@hotmail.com>
2021-10-14 18:56:51 -0300

    exercicios atualizados

--- Fundamentos/Bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/fixar.js
+++ Fundamentos/Bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/fixar.js
@@ -23,7 +23,7 @@ console.log(result);
 */
 
 //3.
-
+/*
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 
@@ -33,3 +33,91 @@ for (let index = 0; index < numbers.length; index += 1) {
 let media = result / numbers.length
 
 console.log(media);
+*/
+// 4.
+
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
+}
+let media = result / numbers.length
+
+if (media > 20) {
+  console.log("valor maior que 20")
+} else{
+  console.log("valor menor ou igual a 20")
+};
+console.log(media);
+*/
+
+// 5.
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = numbers[0];
+for ( let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maior) {
+    maior = numbers[index];
+  }
+}
+
+console.log(maior);
+*/
+
+ // 6.
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if ( numbers[index] % 2 !== 0) {
+    impar += 1;
+  }
+}
+if (impar === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(impar);
+}
+ //6. bonus
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = [];
+for (let index = 0; index < numbers.length; index += 1) {
+  if ( numbers[index] % 2 !== 0) {
+    impar.push(numbers[index]);
+  }
+}
+  console.log(impar);
+
+
+   // 7.
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let menor = numbers[0];
+
+  for(let index = 0; index < numbers.length; index += 1) {
+    if( menor > numbers[index]) {
+      menor = numbers[index]
+    }
+  }
+  console.log(menor);
+  */
+
+  // 8.
+
+  let result = [];
+
+  for( let index = 0; index <= 25; index += 1) {
+    result.push(index);
+  }
+console.log(result);
+
+
+// 9.
+
+let division = [];
+for (index = 0; index < result.length; index += 1) {
+  division.push(result[index] / 2);
+}
+
+console.log(division);
