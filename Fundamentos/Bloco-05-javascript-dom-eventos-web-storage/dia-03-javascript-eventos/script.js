c875538305a9da4a41b3ee444514e512972466bf
Victor Roesner <victorroesner@hotmail.com>
2021-10-25 09:56:57 -0300

    calendario

--- Fundamentos/Bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/script.js
+++ Fundamentos/Bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/script.js
@@ -45,5 +45,151 @@ function createDays() {
 
 createDays();
 
+// 2.
+function createFeriados (btnName) {
+  let buttonDiv = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  newButton.innerHTML = btnName;
+  newButton.id = 'btn-holiday';
+  buttonDiv.appendChild(newButton);
+}
+
+createFeriados('Feriados');
+
+// 3.
+
+function holidayColor() {
+  let holidayDays = document.querySelectorAll('.holiday')
+  let holidayButton = document.querySelector('#btn-holiday')
+
+  holidayButton.addEventListener('click', function(){
+    for (i = 0; i < holidayDays.length; i += 1) {
+      if (holidayDays[i].style.backgroundColor === 'blue') {
+        holidayDays[i].style.backgroundColor = 'rgb(238,238,238)'
+      } else {
+        holidayDays[i].style.backgroundColor = 'blue'
+      }
+    }
+  })
+};
+
+holidayColor();
+
+// 4.
+function createSexta (buttonName) {
+  let buttonDiv = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  newButton.innerHTML = buttonName;
+  newButton.id = 'btn-friday';
+  buttonDiv.appendChild(newButton);
+}
+
+createSexta('Sexta-feira');
+
+// 5.
+
+function fridayText() {
+  let fridayDays = document.querySelectorAll('.friday')
+  let fridayButton = document.querySelector('#btn-friday')
+
+  fridayButton.addEventListener('click', function(){
+    for (i = 0; i < fridayDays.length; i += 1) {
+      if (fridayDays[i].style.fontFamily === 'arial') {
+        fridayDays[i].style.fontFamily = 'verdana'
+      } else {
+        fridayDays[i].style.fontFamily = 'arial'
+      }
+    }
+  })
+};
+
+fridayText()
+
+// 6.
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+
+dayMouseOver();
+dayMouseOut();
+
+// 7.
+function newTaskSpan(task) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  tasksContainer.appendChild(taskName);
+};
+
+newTaskSpan('Projeto:');
+
+// 8.
+
+function newTaskDiv(color) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div');
+
+  newTask.className = 'task';
+  newTask.style.backgroundColor = color;
+  tasksContainer.appendChild(newTask);
+};
+
+newTaskDiv('green');
+
+// 9.
+
+function setTaskClass() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+
+  myTasks.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+};
+
+setTaskClass();
+
+// .10
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+
+setDayColor();
+
 
 
