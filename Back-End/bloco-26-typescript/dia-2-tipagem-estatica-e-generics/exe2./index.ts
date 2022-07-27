6e09fdc4d82c5fc742368f5fc149687463a8dbef
Victor Roesner <victorroesner@hotmail.com>
2022-07-27 18:34:17 -0300

    Exercicio2

---
+++ Back-End/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/exe2./index.ts
@@ -0,0 +1,22 @@
+import Pizza from "./Pizza";
+
+const calabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8
+}
+
+console.log(calabresa);
+
+const marguerita: Pizza = {
+  flavor: "Marguerita",
+  slices: 6
+}
+
+console.log(marguerita);
+
+const nutella: Pizza = {
+  flavor: "Nutella",
+  slices: 4
+}
+
+console.log(nutella);
